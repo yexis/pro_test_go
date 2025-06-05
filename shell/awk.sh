907756aa43f454bb3db14f6852c61450aa995233
lgy <yex_cug@163.com>
2025-06-05 14:58:45 +0800

    shell

--- shell/awk.sh
+++ shell/awk.sh
@@ -5,4 +5,4 @@ awk -F "\t" '{for(i=1;i<=NF;i++) if($i ~ /^all_t=/){split($i, a, "="); if(a[2] >
 cat * | head -100 | awk -F "\t" '{for(i=1;i<=NF;i++) if($i ~ /^all_t=/){split($i, a, "="); print a[2],"+++ "$0}}' | sort -n
 
 # 按照日志中 all_t 的数值进行排序 +++是分隔符 （不显示排序字段）
-cat * | head -100 | awk -F "\t" '{for(i=1;i<=NF;i++) if($i ~ /^all_t=/){split($i, a, "="); print a[2],"+++ "$0}}' | sort -n
+cat * | head -100 | awk -F "\t" '{for(i=1;i<=NF;i++) if($i ~ /^all_t=/){split($i, a, "="); print a[2],"+++ "$0}}' | sort -n | awk -F"+++" '{print $2}'
